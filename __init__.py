c9018da91fba075e0bd36b1d10dfd0337cd3f375
Arthur-Milchior <arthur@milchior.fr>
2019-01-21 20:12:43 +0100

    more informations

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from anki.collection import _Collection
+from anki.consts import *
 from anki.lang import _
 from anki.utils import ids2str
 
@@ -7,14 +8,18 @@ def emptyCardReport(self, cids):
     rep = []
     for ords, mid, flds in self.db.all("""
     select group_concat(ord), mid, flds from cards c, notes n
-    where c.nid = n.id and c.id in %s group by nid""" % ids2str(cids)):
+    where c.nid = n.id and c.id in %s group by nid order by mid""" % ids2str(cids)):
         model = mw.col.models.get(mid)
         templates = model["tmpls"]
-        rep.append(_("Empty cards")+": ")
-        for ord in ords.split(","):
-            ord = int(ord)
-            templateName = templates[ord]["name"]
-            rep.append(templateName+", ")
+        isCloze = model["type"] == MODEL_CLOZE
+        rep.append(_("Empty cards")+" ("+modelName+"): ")
+        if isCloze:
+            rep.append(ords)
+        else:
+            for ord in ords.split(","):
+                ord = int(ord)
+                templateName = templates[ord]["name"]
+                rep.append(templateName+", ")
         rep.append("\nFields: %(f)s\n\n" % dict(f=flds.replace("\x1f", " / ")))
     return "".join(rep)
 
